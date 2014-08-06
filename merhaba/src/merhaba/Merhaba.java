e486190dd90f9cac87451850bc0f018b447ed19e
Seckin Celik <seckin.celik@unit.com.tr>
2014-08-06 09:41:03 +0300

    advanced

--- merhaba/src/merhaba/Merhaba.java
+++ merhaba/src/merhaba/Merhaba.java
@@ -16,7 +16,7 @@ public class Merhaba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("seckin ");
+        System.out.println("seckin celik");
     }
     
 }
